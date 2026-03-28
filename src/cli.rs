7a23b6aeb4201a3697fcb9c787aef604ea479499
Matthew A. Miller <linuxwolf@outer-planes.net>
2026-03-28 14:54:36 -0600

    chore(cli): setup cli framework (#6)

---
+++ src/cli.rs
@@ -0,0 +1,48 @@
+use clap::Parser;
+
+use std::ffi::OsString;
+
+#[derive(Debug, Parser, PartialEq, Eq)]
+#[command(version, about, long_about = None)]
+pub struct Cli {}
+
+impl Cli {
+    pub fn open_with<I, T>(itr: I) -> Cli
+    where
+        I: IntoIterator<Item = T>,
+        T: Into<OsString> + Clone,
+    {
+        Cli::parse_from(itr)
+    }
+
+    pub fn open() -> Cli {
+        Cli::open_with(std::env::args())
+    }
+}
+
+#[cfg(test)]
+mod testing {
+    use clap::CommandFactory;
+
+    use super::*;
+
+    #[test]
+    fn validate_cli() {
+        Cli::command().debug_assert();
+    }
+
+    #[test]
+    fn open_with_no_args() {
+        let args: Vec<String> = Vec::new();
+        let result = Cli::open_with(args);
+
+        assert_eq!(result, Cli {});
+    }
+
+    #[test]
+    fn open_default() {
+        let result = Cli::open();
+
+        assert_eq!(result, Cli {});
+    }
+}
--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,9 @@
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum Error {}
+pub enum AppError {
+    #[error("problem with command-line: {0}")]
+    CliError(#[from] clap::Error),
+}
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type AppResult<T> = std::result::Result<T, AppError>;
