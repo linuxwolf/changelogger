16dacf7e273596f5bf45dff22ee518abeaebe70b
Matthew A. Miller <linuxwolf@outer-planes.net>
2026-03-30 18:11:58 -0600

    chore(logging): setup logging (#7)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use crate::{cli::Cli, errors::AppResult};
+use log::{debug, error, info, warn};
+
+use crate::{cli::Cli, errors::AppResult, logging::AppLogger};
 
 mod cli;
 mod errors;
+mod logging;
 
 fn main() -> AppResult<()> {
-    let _cli = Cli::open();
+    let cli = Cli::open();
+    AppLogger::<termcolor::StandardStream>::init(&cli);
+
+    info!("changelogger starting ...");
+    debug!("setup configuration");
+    debug!(
+        "read version from <default-branch>: `git cat-file --textconv <default-branch>:<version-file>"
+    );
+    error!("fails if <version-file> on <default-branch> does not exist!");
+    debug!("find matching tag: `git tag | grep <version-prefix><current-version>`");
+    debug!("find all commits from <current-version-tag> to <default-branch>");
+    debug!("process commits");
+    debug!(
+        "read current changelog from <default-branch>: `git cat-file --textconv <default-branch>:<changelog-file>"
+    );
+    warn!("initialize with a template if <changelog-file> on <default-branch> does not exist!");
+    debug!("write to <changelog-file>");
+    debug!("write to <version-file>");
 
     Ok(())
 }
